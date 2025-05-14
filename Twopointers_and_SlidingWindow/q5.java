ed5f77e53d31e756a6f0bc9a950bf94dc488afd8
Saicharan-R25 <saicharan.r2003@gmail.com>
2025-05-14 23:51:53 +0530

    q5 own code

---
+++ Twopointers_and_SlidingWindow/q5.java
@@ -0,0 +1,53 @@
+public class q5 {
+    public static void main(String[] args) {
+        int arr[]={1,2,1,2,1,2,1,3};
+        int l=0;int r=0;
+        int b1_val=arr[0]; int b1_counter=0;
+        int b2_val=-1; int b2_counter=0;
+        int maxlen=0; int n=arr.length;
+
+        while(r<=n-1){
+            if(arr[r]!=b1_val && b2_val==-1){
+                b2_val=arr[r];
+                b2_counter=1;
+                b1_counter=r-l;
+                r++;
+                break;
+            }
+            r++;
+        }
+
+        if(b2_counter==0){
+            maxlen=n;
+        }else{
+            
+            while (r<=n-1) {
+                if (arr[r]==b1_val) {
+                    b1_counter++;
+                    r++;
+                }
+                else if(arr[r]==b2_val){
+                    b2_counter++;
+                    r++;
+                }else{
+                    b1_counter=0;
+                    l=r-1;
+                    while (arr[l]==b2_val) {
+                        b1_counter++;
+                        l--;
+                    }
+                    b1_val=b2_val;
+                    b2_val=arr[r];
+                    b2_counter=1;
+                    r++;
+                }
+
+                maxlen=Math.max(maxlen, b1_counter+b2_counter);
+            }
+
+
+        }
+
+        System.out.println(maxlen);
+    }
+}
