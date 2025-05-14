d298d6b4d2593f51769e85109886942c4dffcbae
Saicharan-R25 <saicharan.r2003@gmail.com>
2025-05-14 09:58:13 +0530

    new q4

---
+++ Twopointers_and_SlidingWindow/q4.java
@@ -0,0 +1,26 @@
+public class q4 {
+    /*
+     * max length of a subarray with 1's 
+     * u can flip any K no. zeros to 1 in order to increase the 1's 
+     */
+
+     public static void main(String[] args) {
+        int[] arr={1,1,1,0,0,0,1,1,1,1,0}; int k=2;
+        int l=0;int r=0;int z_c=0;
+        int maxlen=0;
+        while (r<=arr.length-1) {
+            if(arr[r]==0){
+                z_c++;
+            }
+            while (z_c>k) {
+                if(arr[l]==0){
+                    z_c--;
+                }
+                l++;
+            }
+            maxlen=Math.max(maxlen, r-l+1);
+            r++;
+        }
+        System.out.println(maxlen);
+     }
+}
